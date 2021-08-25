287ed9ad33005dc2d25fdf09cef8ce8780beb002
zheng-hao-hao <z20090607@gmail.com>
2021-08-25 23:12:52 +0800

    2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord.flags import Intents
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='!',intents = intents)
+bot = commands.Bot(command_prefix='~',intents = intents)
 
 @bot.event
 async def on_ready():
@@ -22,6 +22,10 @@ async def on_member_join(user):
 async def on_member_remove(user):
     channel = bot.get_channel(875400740015849533)
     await channel.send(f"{user}離開了伺服器!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"目前延遲為{round(bot.latency*1000)} ms")
 
 
 bot.run("ODc5OTIwMzAwMTI3NTU1NTg0.YSWvow.9krQ3kWzpsqgFeGfh7YrOaCKxI8")
