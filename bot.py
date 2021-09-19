48db0c931632ca737d5a2f86bf98084e4d812733
zheng-hao-hao <z20090607@gmail.com>
2021-09-19 14:10:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ with open('setting.json','r',encoding='utf8') as jfile:
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='~',intents = intents)
+bot = commands.Bot(command_prefix='/',intents = intents)
 
 @bot.event
 async def on_ready():
